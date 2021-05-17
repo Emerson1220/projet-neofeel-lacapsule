bc0ec7b571d22b2a6e58e2a44ebd6d02dadcc57d
Pierre M <ferrandpierremarie@gmail.com>
2021-05-17 17:49:12 +0200

    ajout des routes + fin de navBar + Router OP

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import Nav from './Nav'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ScreenHome from './ScreenHome';
 function App() {
   return (
     
-      <Nav>
+      <Router>
+        <Switch>
+          <Route component={ScreenHome} path="/" exact />
+          <Route component={ScreenConnexion} path="/connexion"  />
+          <Route component={ScreenSearch} path="/recherche"  />                    
+          <Route component={ScreenInfo} path="/info"  />
+          <Route component={ScreenPartner} path="/partenaire"  />
+          <Route component={ScreenRoadPlanner} path="/roadPlanner"  />
+          <Route component={ScreenTrips} path="/voyages"  />
+          <Route component={ScreenProfil} path="/profil"  />
+          <Route component={ScreenBasket} path="/panier"  />
+          <Route component={ScreenSuggestion} path="/suggestion"  />
+          
+        </Switch>
+      </Router>
 
-      </Nav>
-    
   );
 }
 
---
+++ reactapp/src/ScreenHome.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'antd';
+import {Link} from 'react-router-dom';
+import Nav from './Nav'
+
+
+function ScreenHome() {
+    return(
+            <Nav/>
+
+
+    )
+} 
+
+
+export default ScreenHome;
