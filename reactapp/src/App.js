c81ef5e61b5151686fd1ba1e5e5ca421b818a773
Anais Moutarlier <anaismoutarlier@gmail.com>
2021-05-17 15:07:16 +0200

    merge nav

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import SearchPage from './SearchPage';
 
 function App() {
   return (
     <SearchPage></SearchPage>
-    // <Test/>
+
   );
 }
 
