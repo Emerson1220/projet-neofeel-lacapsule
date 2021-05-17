c0de8aa29b3ebfd7e7621b8f6584f3673baf0c78
Pierre M <ferrandpierremarie@gmail.com>
2021-05-17 14:47:01 +0200

    travail sur NAVBar 60%

--- reactapp/src/Nav.js
+++ reactapp/src/Nav.js
@@ -1,21 +1,49 @@
 import React from 'react';
 import './App.css';
-import {Menu, Icon, Image} from 'antd'
+
+import { Button } from 'antd';
 
 
-function  Nav() {
+
+function Nav() {
 
     return (
-        <nav>
-            <div style={{position:absolute}}>                
-                    <img
-                    width ={'332px'}
-                    height ={'117px'}
-                    
-                    src= "./public/imageNav/LOGO.png" />
-                
+        <nav style={{ width: '100vh', display: 'flex', flexDirection: 'row' }}>
+            <div>
+                <img
+                    width={'332px'}
+                    height={'117px'}
+                    src="/images/LOGO.png" />
+
+            </div>
+            <div style={{ display: "flex", flexDirection: 'column' }}>
+                <img
+                    width={'38px'}
+                    height={'36px'}
+                    style={{ marginTop: '5px', marginLeft: '10px' }}
+                    src="/images/facebookColor.png" />
+                <img
+                    width={'38px'}
+                    height={'36px'}
+                    style={{ marginTop: '5px', marginLeft: '10px', color: '#FF0000' }}
+                    src="/images/youtubeColor.png" />
+
+                <img
+                    width={'38px'}
+                    height={'36px'}
+                    style={{ marginTop: '5px', marginLeft: '10px' }}
+                    src="/images/instagramColor.png" />
+            </div>
+            <div style={{}}>
+                <Button className ='devenezPartenaireButton' > Devenez Partenaire </Button>
+            </div>
+            <div className='listNav'>
+                <h3 style={{color: '#106271' }}>Mon Voyage</h3>
+                <h3 style={{color: '#106271' }}>Recherche</h3>
+                <h3 style={{color: '#106271' }}>Connexion</h3>
             </div>
 
         </nav>
     )
 }
+export default Nav;
