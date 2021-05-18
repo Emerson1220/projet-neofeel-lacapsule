4f6edc2f84ea936ad7728e21bb8c112d1017fda4
Pierre M <ferrandpierremarie@gmail.com>
2021-05-18 10:12:58 +0200

    recalibration des routes, push et mise a jour

--- reactapp/src/ScreenHome.js
+++ reactapp/src/ScreenHome.js
@@ -8,26 +8,27 @@ import { icons } from 'antd/lib/image/PreviewGroup';
 
 function ScreenHome() {
     return (
-        <div style={{ display: "flex", flexDirection: 'column' }}>
+        <div style={{ display: "flex", flexDirection: 'column', height: '100%' }}>
             <Nav />
-            <div style={{ display: "flex", flexDirection: 'column', height: '100%', position: 'relative' }} >
+            <div style={{ display: "flex", flexDirection: 'column', height: '100%' }} >
                 <div style={{
                     backgroundImage: `url("images/BanniereHome.jpg")`,
                     backgroundPosition: 'center',
                     backgroundSize: 'cover',
                     backgroundRepeat: 'no-repeat',
                     display: 'grid',
-                    gridTemplateColumns: 'repeat(2, 1fr)'
+                    gridTemplateColumns: 'repeat(2, 1fr)',
+
                 }} >
                     <div style={{
-                        width: '34%',
-                        height: '200px',
+                        width: '70%',
+                        height: '100%',
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
                         justifyContent: 'center',
                         backgroundColor: 'rgba(16, 98, 113, 0.3)',
-                        position: 'relative'
+
                     }}>
                         <h5 style={{ color: '#FFF', marginLeft: '6%', marginRight: '3%' }}>
                             Neofeel développe une tendance innovante du tourisme éthique.
@@ -35,30 +36,34 @@ function ScreenHome() {
                         </h5>
                         <div style={{ display: 'flex', flexDirection: 'row', height: '35%', width: '80%', justifyContent: 'space-around', alignItems: 'center' }}>
                             <img
-                                style={{ height: '55%', width: 'auto', borderRadius: 3 }}
+                                style={{ height: '45%', width: 'auto', borderRadius: 3, marginRight: '3%' }}
                                 src='/images/neopassRecto.png'
                             />
                             <h6 style={{ color: '#FFF' }}>
                                 Bénéficiez d’avantages <br />particuliers avec le Neopass...
                             </h6>
                         </div>
-                        <div>
-                            <Button className='ensavoirplusButton' > En savoir plus.. </Button>
+                        <div style={{paddingTop:'1%'}}>
+                            <Link to ={'/info'}>
+                            <Button className='ensavoirplusButton' > Le Concept   </Button>
+                            </Link>
                         </div>
                     </div>
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', justifyContent:'flex-end' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', justifyContent: 'flex-end', paddingBottom: '3%', paddingRight:'2%' }}>
+                    <h2 style={{ color: '#FFF', paddingRight: '1%' }}>Découvrez la France autrement...</h2>
+                        <Button className='ensavoirplusButton' >Commencer l'aventure... </Button>
+                        
 
-                        <h2 style={{ color: '#FFF', paddingRight:'1%' }}>Découvrez la France autrement...</h2>
                     </div>
                 </div>
-                <div className ="cards">
+                {/* <div className ="cards">
                     <article className="card">                       
                         <img src="/images/ALSA-CYCLO-TOURS-6-min.jpg" alt="Alsa Cyclo Tours"></img>
                     </article>
                     <article className="card">
                         <img src="/images/domaines-schlumberger-entree-min.jpg.webp" alt="Alsa Cyclo Tours"></img>
                     </article>
-                </div>
+                </div> */}
 
             </div>
         </div>
