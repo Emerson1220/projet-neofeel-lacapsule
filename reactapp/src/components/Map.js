1236bd1160612c852c4d89254b7030ba2e660980
Emerson1220 <74653664+Emerson1220@users.noreply.github.com>
2021-05-25 12:16:08 +0200

    debeug roadplanner anais

--- reactapp/src/components/Map.js
+++ reactapp/src/components/Map.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../App.css';
 import '../styles/googleMap.css';
-import { Link } from 'react-router-dom';
 
 //MAP
 import GoogleMapReact from 'google-map-react';
@@ -12,208 +11,26 @@ import { connect } from 'react-redux';
 //UI
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMapMarker } from '@fortawesome/free-solid-svg-icons'
-import { Modal, Cascader, message  } from 'antd';
-import RedButton from './RedButton';
-
+import { Popover } from 'antd';
+import PopoverContent from './PopoverContent';
 
 const Map = (props) => {
-    const [experience, setExperience] = useState({ partner: { addresses: [{ city: '' }] }, tags: [], description: {imageBannerUrl:''} })
-    const [visible, setVisible] = useState(false);
-    const [voyageSelect, setVoyageSelect] = useState('');
-
-    //MODAL
-    const showModal = (exp) => {
-        setExperience(exp)
-        setVisible(!visible);
-    
-    };
-
-    const handleCancel = () => {
-        setVisible(!visible);
-    };
-
-    //CASCADER
-    let options = [
-            {
-                value: 'new',
-                label: 'Mon voyage'
-            }
-        ]
-        
-    if (props.user.roadtrips) { 
-        let userTrips = props.user.roadtrips.map(e => {
-            return {
-                value: e._id,
-                label: e.name
-            }
-        });
-
-        let savedTripsOption = {};
-
-        if (userTrips.length > 0) { 
-            savedTripsOption = {
-                value: 'saved',
-                label: "Vos voyages",
-                children: userTrips
-            }
-        }
-
-        options = [
-        {
-            value: 'new',
-            label: 'Nouveau voyage'
-        },
-        savedTripsOption
-    ];
-    }
-
-    function displayRender(label) {
-        return label[label.length - 1];
-    }
-
-    const onChange = (value) => {
-        setVoyageSelect(value)
-    };
-
-    //HTTP REQUESTS
-const createNewTrip = async (experience) => {
-    let region = 'Alsace-Vosges';
-        let rawResponse = await fetch('/myroadplanner', {
-            method: "POST",
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            body: `token=${props.user.token}&name=Mon Voyage en ${region}&region=${region}&regionCode=${props.region}&experienceID=${experience._id}`
-        });
-        let response = await rawResponse.json();
-        if (response.result === true) {
-            props.toggleRoadplanner(response.roadtrip._id, experience);
-            props.addRoadtripToUser(response.roadtrip)
-            success();
-        }
-    }
-    
-    const addExperienceToTrip = async(experience) => {
-        let rawResponse = await fetch('/myroadplanner', {
-            method: 'PUT',
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            body: `roadtripID=${voyageSelect[1]}&experienceID=${experience._id}`
-        })
-        let response = await rawResponse.json();
-        if(response.result === true) {
-            props.addExperience(response.roadtrip._id, experience)
-            success()
-        }
-    }
-
-    //FUNCTIONS
-    const chooseExperience = async(experience) => {
-        if (props.user.token) {
-            voyageSelect[0] === 'new' ? createNewTrip(experience) : addExperienceToTrip(experience);
-        } else {
-            !props.roadplanner.experiences || props.roadplanner.experiences.length === 0 ? props.toggleRoadplanner('temp', experience) : props.addExperience('temp', experience) ;
-            success();
-        }
-    }
-
-    const success = () => {
-        message.success({
-            content: 'Expérience ajouté',
-            className: 'custom-class',
-            style: {
-                marginTop: '20vh',
-            },
-        });
-    };
-
-    let openModal = [];
-    if (experience !== {}) {
-        openModal =
-        <div style={styles.single_destinations}> 
-        <div style={styles.image_card}>
-            <img style={styles.image} src={ experience.description.imageBannerUrl ? experience.description.imageBannerUrl : "images/photo-526x360.png" } alt="list" />
-        </div>
-        <div style={{ width: '100%',backgroundColor: 'white',display:'flex', flexWrap:'nowrap', padding:'1rem'}}>
-            {experience.tags.map((image, j) => {
-                return (<img key={j} style={styles.picto} src={`images/pictos/${image}-8.png`} alt={image} />)
-            }) }
-        </div>
-            <div style={styles.detail_title_location}>
-                <div>
-                    <Link
-                    style={styles.h3}
-                    to={{
-                        pathname: `/partenaire/${experience._id}`,
-                        state: {
-                            experience: experience
-                        }
-                    }}>
-                        <h3>{experience.name}</h3>
-                    </Link>
-                    <Link
-                    style={styles.h4}
-                    to={{
-                        pathname: `/partenaire/${experience._id}`,
-                        state: {
-                            experience: experience
-                        }
-                    }}>
-                        <h4>{experience.subtitle}</h4>
-                    </Link>
-                </div>
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems:'flex-end' }}>
-                    <p style={{ color: '#e06868', marginBottom: '8px' }}>
-                    <img style={{ marginRight: '4px' }} src="images/icone-geo.png" alt="map" />{experience.region}</p>
-                    <h4 ><Link style={styles.h4} to="/">{experience.city}</Link></h4>
-                </div>
-            </div>
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <p style={{ color: '#e06868', marginBottom: '8px' }}>
-                <img style={{ marginRight: '4px' }} src="images/icone-geo.png" alt="map" />{experience.region}</p>
-                <h4 >{experience.partner.addresses[0].city}</h4>
-            </div>
-        <div style={styles.detail_card}>
-                <div style={styles.liste_temps_item}>
-                    <p>Temps</p>
-                    <h2>{experience.activityTime}</h2>
-                </div>
-                <div style={styles.liste_price_item}>
-                    <p>Prix</p>
-                    <h2>{experience.budget}</h2>
-                </div>
-        </div>
-        <div style={{ textAlign: 'center', marginTop:'1rem', alignSelf: 'center', width: '100%' }}>
-            <h4 style={styles.h4}>Ajouter cette experience à votre voyage</h4>
-            <Cascader
-            options={ options }
-            expandTrigger="hover"
-            displayRender={ displayRender }
-            onChange={ onChange }
-            placeholder="Sélectionnez un voyage"
-            />
-            <RedButton
-            title="+"
-            onSelect={ ()=>chooseExperience(experience) }/>
-        </div>
-    </div>
-    }
-
-    const LocationPin = (props) => (
-        <div className="pin" onClick={() => props.onSelect()}>
-            <FontAwesomeIcon icon={faMapMarker} className="pin-icon" />
-            <p className="pin-text">{ props.text }</p>
-        </div>
-    );
-
     var ExperienceListingMap = [];
     if (props.experiences) {
 
         ExperienceListingMap = props.experiences.map((experience, i) => {
             return (
-                <LocationPin
-                    key={i}
-                    onSelect={() => showModal(experience)}
-                    lat={experience.coordinate.latitude}
-                    lng={experience.coordinate.longitude}
-                />
+                <Popover
+                key={i}
+                content={ <PopoverContent experience={ experience } /> }
+                
+                trigger='click'                       
+                lat={experience.coordinate.latitude}
+                lng={experience.coordinate.longitude}>
+                    <div className="pin">
+                        <FontAwesomeIcon icon={faMapMarker} className="pin-icon" />
+                    </div>
+                </Popover>
 
             )
         })
@@ -236,18 +53,6 @@ const createNewTrip = async (experience) => {
                     {ExperienceListingMap}
                 </GoogleMapReact>
             </div>
-            <Modal
-                title=''
-                centered={true}
-                visible={visible}
-                footer={null}
-                closable={ false }
-                bodyStyle={styles.modal}
-                maskStyle={styles.modalMask}
-                onCancel={() => handleCancel()}>
-
-                {openModal}
-            </Modal>
         </div>
 
     )
@@ -313,7 +118,8 @@ let styles = {
         borderRadius: '7px',
         position: 'relative',
         overflow: 'hidden',
-        width: '100%',
+        width: '30vw',
+        height: '50vh'
     },
 
     image_card:{
@@ -376,13 +182,9 @@ let styles = {
         marginTop: '-3rem',
     },
 
-    modal: {
+    popover: {
         backgroundColor: 'rgba(244, 244, 246, 0.5)',
         borderRadius: '15px',
         height: '70%'
-    },
-
-    modalMask: {
-        backgroundColor: "rgba(133, 187, 197, 0.6)"
     },
 }
